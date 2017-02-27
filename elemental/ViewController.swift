b6e0c9eb04e200454960f9a944b7a8efe36a0de0
jen-nordell <jennifer.nordell@outlook.com>
2017-02-27 19:32:00 +0100

    Initial Commit

---
+++ elemental/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  elemental
+//
+//  Created by Jennifer Nordell on 2017-02-27.
+//  Copyright © 2017 Jennifer Nordell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
