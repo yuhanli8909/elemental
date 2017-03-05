e3792377e7f17cfa61b08372de841dcfd916683d
jen-nordell <jennifer.nordell@outlook.com>
2017-03-05 16:21:35 +0100

    Commenting of ViewController.swift

--- elemental/ViewController.swift
+++ elemental/ViewController.swift
@@ -32,19 +32,21 @@ class ViewController: UIViewController {
       displayCorrectMessage(isCorrect: false)
     }
     
+    
+    // Avoiding out of bounds by ensuring that the index is not incremented if it's on the last challenge
+    
     if quiz.currentChallengeIndex < quiz.challenges.count - 1 {
       quiz.currentChallengeIndex += 1
       loadNextRoundWithDelay(seconds: 2)
     } else {
       for button in choiceButtons {
         button.isHidden = true
-      } 
+      }
       scoreInformation.text = quiz.quizScore()
       scoreInformation.isHidden = false
       gameInformation.isHidden = false
       playAgainButton.isHidden = false
     }
-    
   }
   
   @IBAction func playAgain(_ sender: Any) {
@@ -53,7 +55,6 @@ class ViewController: UIViewController {
   }
   
   // Create an instance of quiz and set the current index of
-  // the collection of challenges to 0
   
   private var quiz = Quiz()
 
@@ -69,6 +70,7 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  // Display if the answer was correct or not
   
   func displayCorrectMessage(isCorrect: Bool) {
     for button in choiceButtons {
@@ -82,6 +84,9 @@ class ViewController: UIViewController {
     }
   }
   
+  
+  // Display all important information about the Quiz
+  
   func displayQuizInformation() {
     gameInformation.isHidden = true
     playAgainButton.isHidden = true
@@ -92,10 +97,15 @@ class ViewController: UIViewController {
   }
   
   
+  // Display the question and the type of question 
+  
   func displayQuestionInformation() {
     challengeInformation.text = quiz.questionText
     challengeType.text =  quiz.questionTypeText
   }
+  
+  
+  // Display the choice to pick from including a guaranteed correct answer
   
   func displayChoices() {
     var choiceIndex: Int = 0
@@ -105,7 +115,16 @@ class ViewController: UIViewController {
       choiceIndex += 1
     }
     
+    
+    // Logic to show 3 choices instead of the customary 4 to meet the exceeds expectations criteria
+    
+    // If the number of choices to display is 3.  I could have not used a "magic number" here, but felt
+    // it was appropriate for clarity
+    
     if quiz.choicesToDisplay == 3 {
+      // If the first text assigned to the button was equal to the correct answer
+      // hide the last button, otherwise hide the first button
+      
       if quiz.answerText == quiz.answerTexts.first! {
         choiceButtons.last!.isHidden = true
       } else {
@@ -114,6 +133,9 @@ class ViewController: UIViewController {
     }
   }
 
+  
+  // Display how many correct out of the total number of questions available
+  
   func displayGameProgress() {
     gameProgress.text = "Correct: \(quiz.numberCorrect)/\(quiz.numberOfQuestions)"
   }
